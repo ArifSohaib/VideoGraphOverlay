1131f3a784bd3d6e7108184012761997240c0196
Sohaib <arif_sohaib@outlook.com>
2018-07-10 12:31:38 -0400

    graph made using FigureCanvas, video saving works

--- testOverlay.py
+++ testOverlay.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import numpy as np
 import time
 import sys
@@ -12,17 +13,23 @@ import cv2
 outputFile = 'sample_output.avi'
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(outputFile, fourcc, 30.0, (1920, 1080))
+out = cv2.VideoWriter(outputFile, fourcc, 30.0, (640, 480))
 #frames = int(cap.get(cv2.CV_CAP_PROP_FRAME_COUNT))
 #width  = int(cap.get(cv2.CV_CAP_PROP_FRAME_WIDTH))
 #height = int(cap.get(cv2.CV_CAP_PROP_FRAME_HEIGHT))
 
-fig, ax = plt.subplots(1,1)
+fig = plt.figure()
+canvas = FigureCanvas(fig)
+ax1 = fig.add_subplot(2,2,4)
+ax2 = fig.add_subplot(2,2,3)
 plt.ion()
 plt.show()
-width = 1920
-countDown = 5
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 fps = int(cap.get(cv2.CAP_PROP_FPS))
+print("Recording at width: {}, width: {}, fps: {}".format(width, height, fps))
+countDown = 5
+
 frames = countDown *fps
 #Setup a dummy path
 x = np.linspace(0,width,frames)
@@ -31,15 +38,18 @@ start_time = time.time()
 time_elapsed = 0
 for i in range(frames):
     fig.clf()
-    flag, frame = cap.read()
+    # flag, frame = cap.read()
 
-    plt.imshow(frame)
-    plt.plot(x,y,'k-', lw=2)
-    plt.plot(x[i],y[i],'or')
-    plt.show()
+    # plt.imshow(frame)
+    ax1.plot(x,y,'k-', lw=2)
+    ax2.plot(x[i],y[i],'or')
+    canvas.draw()
+    image = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
+    image = np.reshape(image, (480,640,3))
+    out.write(image)
     time_elapsed = time.time() - start_time
     print(time_elapsed)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        cap.release()
         break
+cap.release()
+cv2.destroyAllWindows()
