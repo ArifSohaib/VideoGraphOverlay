421dc5666713d255fd6fe932ec7ed06a39f702a1
Sohaib <arif_sohaib@outlook.com>
2018-07-10 13:06:15 -0400

    video output working, graph display working

--- testOverlay.py
+++ testOverlay.py
@@ -1,15 +1,31 @@
 
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 import numpy as np
 import time
 import sys
-try:
-    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-except:
-    pass
+
 import cv2
-#filename = './SampleVideo.mp4'
+
+
+def fig2data(fig):
+    """
+    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
+    @param fig a matplotlib figure
+    @return a numpy 3D array of RGBA values
+    """
+    # draw the renderer
+    fig.canvas.draw()
+
+    # Get the RGBA buffer from the figure
+    w, h = fig.canvas.get_width_height()
+    buf = np.fromstring(fig.canvas.tostring_argb(), dtype=np.uint8)
+    buf.shape = (w, h, 4)
+
+    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+    buf = np.roll(buf, 3, axis=2)
+    return buf
+
 outputFile = 'sample_output.avi'
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -18,12 +34,11 @@ out = cv2.VideoWriter(outputFile, fourcc, 30.0, (640, 480))
 #width  = int(cap.get(cv2.CV_CAP_PROP_FRAME_WIDTH))
 #height = int(cap.get(cv2.CV_CAP_PROP_FRAME_HEIGHT))
 
-fig = plt.figure()
+fig = Figure()
+fig.set_rasterized(True)
 canvas = FigureCanvas(fig)
 ax1 = fig.add_subplot(2,2,4)
 ax2 = fig.add_subplot(2,2,3)
-plt.ion()
-plt.show()
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 fps = int(cap.get(cv2.CAP_PROP_FPS))
@@ -37,15 +52,15 @@ y = x/2. + 100*np.sin(2.*np.pi*x/1200)
 start_time = time.time()
 time_elapsed = 0
 for i in range(frames):
-    fig.clf()
-    # flag, frame = cap.read()
+    flag, frame = cap.read()
 
-    # plt.imshow(frame)
     ax1.plot(x,y,'k-', lw=2)
     ax2.plot(x[i],y[i],'or')
-    canvas.draw()
-    image = np.fromstring(canvas.tostring_rgb(), dtype='uint8')
-    image = np.reshape(image, (480,640,3))
+    fig.tight_layout()
+    image = fig2data(fig)
+    image = cv2.resize(image,(640,480))
+    image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
+    cv2.imshow('graph',image)
     out.write(image)
     time_elapsed = time.time() - start_time
     print(time_elapsed)
