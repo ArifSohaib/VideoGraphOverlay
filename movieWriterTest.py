b6761bfc0b121b99bfdeae3935d22e1b9db1862c
Sohaib <arif_sohaib@outlook.com>
2018-07-10 15:07:03 -0400

    matplot writer giving good initial output

--- movieWriterTest.py
+++ movieWriterTest.py
@@ -16,6 +16,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
+import cv2
 
 FFMpegWriter = manimation.writers['ffmpeg']
 metadata = dict(title='Movie Test', artist='Matplotlib',
@@ -23,16 +24,21 @@ metadata = dict(title='Movie Test', artist='Matplotlib',
 writer = FFMpegWriter(fps=15, metadata=metadata)
 
 fig = plt.figure()
+ax = fig.add_subplot(2,1,2)
 l, = plt.plot([], [], 'k-o')
 
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
 
 x0, y0 = 0, 0
-
+cap = cv2.VideoCapture(0)
 with writer.saving(fig, "writer_test.mp4", 100):
     for i in range(100):
         x0 += 0.1 * np.random.randn()
         y0 += 0.1 * np.random.randn()
         l.set_data(x0, y0)
+        ax2 = fig.add_subplot(2,1,1)
+        ok, img = cap.read()
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        ax2.imshow(img)
         writer.grab_frame()
